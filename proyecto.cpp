fe76cacc68bb14bd5607d322a4f6f2d9ae49d536
isabella musicue <isamusicue30@gmail.com>
2025-10-01 19:20:52 -0500

    comic inicial

---
+++ proyecto.cpp
@@ -0,0 +1,44 @@
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+#include <Adafruit_GFX.h>
+#define PANEL_WIDTH     64
+#define PANEL_HEIGHT    32
+#define PANELS_NUMBER   1
+HUB75_I2S_CFG mxconfig(
+    PANEL_WIDTH,   // ancho
+    PANEL_HEIGHT,  // alto
+    PANELS_NUMBER  // cantidad de paneles
+);
+MatrixPanel_I2S_DMA display(mxconfig);
+unsigned long lastChange = 0;
+const unsigned long interval = 1000; // intervalo en milisegundos (1 segundo)
+int currentNumber = 1;
+void setup() {
+  Serial.begin(115200);
+  
+  if (!display.begin()) {
+    Serial.println("Error al inicializar el panel");
+    return;
+  }
+
+  display.setBrightness8(60); // brillo entre 0 y 255
+  display.clear();
+  display.setTextWrap(false);  // evitar que el texto se corte
+  display.setTextColor(display.color565(255, 255, 255)); // color blanco
+  display.setTextSize(5); // tamaño grande
+}
+void loop() {
+  unsigned long currentTime = millis();
+
+  if (currentTime - lastChange >= interval) {
+    display.fillScreen(0); // limpia pantalla
+
+    display.setCursor(10, 0); // posición del número
+    display.print(currentNumber);
+
+    currentNumber++;
+    if (currentNumber > 7) currentNumber = 1;
+
+    lastChange = currentTime;
+  }
+}
+
